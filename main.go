17b33560d37f4e9b9d9561f56b39060eaa468462
alexpo <sashactoirs@gmail.com>
2020-09-21 17:15:06 +0300

    2 lessons

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"os"
 )
 
-//  создаем кастомные типы 
-type Celsius float32
-type Fahrenheit float32
-type EnvConfig struct {   // создаем новый тип данных, структуру (объект), в который будем читать переменные окружения
+    //  создаем кастомные типы 
+    type Celsius float32
+    type Fahrenheit float32
+    type EnvConfig struct {   // создаем новый тип данных, структуру (объект), в который будем читать переменные окружения
 	LogFile string `envconfig:"LOG_FILE"`
 }
 
@@ -17,13 +17,13 @@ type EnvConfig struct {   // создаем новый тип данных, ст
 func main() {
 	var eConf EnvConfig       // создаем переменную
 
-// вызываем функцию Process пакета envconfig
-// передаем в нее в качестве аргумента префикс (пустая строка, нет префикса), и адрес переменной, в которую нужно прочитать переменные окружения 
-envconfig.Process("", &eConf)
+    // вызываем функцию Process пакета envconfig
+    // передаем в нее в качестве аргумента префикс (пустая строка, нет префикса), и адрес переменной, в которую нужно прочитать переменные окружения 
+    envconfig.Process("", &eConf)
 
 
-// вызываем функцию OpenFile пакета os (открывает существующий файл по указанному имени или создает и открывает новый)
-// передаем в нее в качестве аргументов имя файла, флаги файла и права (permissions)
+    // вызываем функцию OpenFile пакета os (открывает существующий файл по указанному имени или создает и открывает новый)
+    // передаем в нее в качестве аргументов имя файла, флаги файла и права (permissions)
     file, _ := os.OpenFile(eConf.LogFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 
     var log = logrus.New() // создаем новый экземпляр логгера
@@ -31,11 +31,11 @@ envconfig.Process("", &eConf)
 	
 
 	c := Celsius(32)
-	logrus.Info("Температура по фаренгейту - ", toFahrenheit(c))
+	log.Info("Температура по фаренгейту - ", toFahrenheit(c))
 }
 
-// функция для перевода температуры по цельсию в фаренгейт - (t*9/5)+32
-func toFahrenheit(t Celsius) Fahrenheit {
+    // функция для перевода температуры по цельсию в фаренгейт - (t*9/5)+32
+    func toFahrenheit(t Celsius) Fahrenheit {
 	var temp Fahrenheit
 	temp = Fahrenheit((t * 9 / 5) + 32)
 
