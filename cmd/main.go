86250013d13584163d088c1228d2878bc6618627
alexpo <sashactoirs@gmail.com>
2020-09-30 10:41:38 +0300

    3 lessons

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,13 @@ package main
 import (
     "github.com/sirupsen/logrus"
     "github.com/kelseyhightower/envconfig" // не убирать и не комментировать, починить ошибку, добавив алиас
+    "github.com/SashaPoGo/Test1/pkg/usecases"
     "os"
 )
 
-    //  создаем кастомные типы 
-    type Celsius float32
-    type Fahrenheit float32
-    type EnvConfig struct {   // создаем новый тип данных, структуру (объект), в который будем читать переменные окружения
+type EnvConfig struct {   // создаем новый тип данных, структуру (объект), в который будем читать переменные окружения
     LogFile string `envconfig:"LOG_FILE"`
 }
-
 
 func main() {
     var eConf EnvConfig       // создаем переменную
@@ -26,13 +23,12 @@ func main() {
     file, _ := os.OpenFile(eConf.LogFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
     var log = logrus.New() // создаем новый экземпляр логгера
     log.Out = file         // перенаправляем его stdout поток в файл (дефолтно в терминал)
-    c := Celsius(32)
-    log.Info("Температура по фаренгейту - ", toFahrenheit(c))
+    
+    tempHistoryHandler := &usecases.HistoryHander{
+		Logger:  log,
+		Samples: []usecases.Fahrenheit{},
+    }
+    usecases.CheckAndSave(tempHistoryHandler, usecases.Celsius(32), 5)
+   
 }
 
-    // функция для перевода температуры по цельсию в фаренгейт - (t*9/5)+32
-    func toFahrenheit(t Celsius) Fahrenheit {
-    var temp Fahrenheit
-    temp = Fahrenheit((t * 9 / 5) + 32)
-    return temp
-} 
